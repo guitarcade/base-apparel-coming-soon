78bc28fe9f71f90f46b45158c58a2aaa63084ba2
Arik Cohen <me@arikcohen.com>
2023-06-19 14:48:34 -0400

    finished mobile version

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
 import Image from "next/image";
 import heroDesktop from "../public/hero-desktop.jpg";
+import heroMobile from "../public/hero-mobile.jpg";
 import logo from "../public/logo.svg";
 import submitIcon from "../public/icon-arrow.svg";
 
 export default function Home() {
   return (
-    <main className="grid grid-cols-[1fr_.8fr]">
-      <div className="flex flex-col py-10 place-self-center">
-        <div className="max-w-[400px]">
-          <Image src={logo} width={158} height={33} alt="" />
-          <div className="grid gap-8">
-            <h1 className="text-6xl uppercase mt-24 tracking-widest leading-[1.1em]">
+    <main className="grid md:grid-cols-[1fr_.8fr]">
+      <div className="flex flex-col py-10 justify-self-center">
+        <div className="md:max-w-[400px]">
+          <Image src={logo} width={158} height={33} alt="" className="px-4" />
+          <Image
+            src={heroMobile}
+            alt=""
+            className="object-cover md:hidden w-full mt-10"
+          />
+          <div className="grid gap-8 text-center md:text-left p-16 md:p-0">
+            <h1 className="text-6xl uppercase md:mt-24 tracking-widest leading-[1.1em]">
               <span className="font-light text-primary-red-1">We're</span>{" "}
               coming soon
             </h1>
@@ -40,13 +46,13 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="">
+      <div className="md:block hidden">
         <div className="h-screen w-full relative">
           <Image
             src={heroDesktop}
             alt=""
             fill={true}
-            className="object-cover"
+            className="object-cover "
           />
         </div>
       </div>
