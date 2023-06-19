687565e6dbd3ec1f4343da00789de57988b8c60b
Arik Cohen <me@arikcohen.com>
2023-06-19 13:43:38 -0400

    working on desktop design

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import heroDesktop from "../public/hero-desktop.jpg";
 import logo from "../public/logo.svg";
+import submitIcon from "../public/icon-arrow.svg";
 
 export default function Home() {
   return (
     <main className="grid min-h-screen place-content-center">
-      <div className="grid grid-cols-2">
-        <div className="flex flex-col items-start px-24 py-10">
+      <div className="grid grid-cols-[1fr_.7fr] max-w-[1440px]">
+        <div className="flex flex-col items-start px-40 py-10">
           <Image src={logo} width={158} height={33} alt="" />
-          <div className="grid gap-4">
-            <h1 className="text-6xl uppercase mt-24 tracking-widest">
+          <div className="grid gap-8">
+            <h1 className="text-6xl uppercase mt-24 tracking-widest leading-[1.1em]">
               <span className="font-light text-primary-red-1">We're</span>{" "}
               coming soon
             </h1>
@@ -18,9 +19,27 @@ export default function Home() {
               store. Add your email below to stay up-to-date with announcements
               and our launch deals.
             </p>
+            <div className="relative">
+              <input
+                placeholder="Email Address"
+                className="w-full p-4 border-primary-red-1/30 border-2 rounded-full placeholder-primary-red-1/50"
+              />
+              <button
+                type="submit"
+                className="bg-primary-red-2/80 px-12 py-4 rounded-full right-0 h-full absolute"
+              >
+                <Image
+                  src={submitIcon}
+                  alt=""
+                  width={12}
+                  height={20}
+                  className=""
+                />
+              </button>
+            </div>
           </div>
         </div>
-        <div className="max-w-full">
+        <div className="">
           <div className="h-screen w-full relative">
             <Image
               src={heroDesktop}
