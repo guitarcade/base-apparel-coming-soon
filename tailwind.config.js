da7df9159560f3ef747395edd56fad99f16df6a4
Arik Cohen <me@arikcohen.com>
2023-06-19 13:12:33 -0400

    wrangling the hero image

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "primary-red-1":"hsl(0, 36%, 70%)",
+        "primary-red-2":"hsl(0, 93%, 68%)",
+        "neutral-red-1":"hsl(0, 6%, 24%)",
       },
     },
   },
